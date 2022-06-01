8d5854b84bfd470fa387bed272a338f04f94b7ff
zRHin <zrhin@rambler.ru>
2022-06-01 16:21:12 +0500

    task solution

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  result = (arr1.length === arr2.length && arr1.every((num, idx) => num === arr2[idx]));
+  return result;
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = (arr.filter((num) => num > 0 && num % 3 === 0).map((num) => num * 10));
   return resultArr; // array
 }
