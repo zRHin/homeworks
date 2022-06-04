1349a0bce576a54679229ac9d0649cd2ac0b5900
zRHin <zrhin@rambler.ru>
2022-06-04 21:21:26 +0500

    task solution

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,34 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this["subject"] = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.unshift(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  this.marks = mark;
+}
+
+Student.prototype.getAverage = function () {
+  let result = 0;
+  this.marks.forEach((item) => result += item);
+  return result / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this["excluded"] = reason;
+}
